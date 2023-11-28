c414b62595cd9a3dcda12eec515f2098b6cc8efa
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-11-28 19:14:08 -0300

    Update list.php

--- client/list.php
+++ client/list.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization");
 
 // files needed to connect to database
 include_once '../config/database.php';
